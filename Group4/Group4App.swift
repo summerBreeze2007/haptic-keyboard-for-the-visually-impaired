a5cea6c3b8cf01c2ccbd0f6a8d6d8755fd6bdcb0
summerBreeze <tae0409@snu.ac.kr>
2025-06-12 23:15:35 +0900

    Initial Commit

---
+++ Group4/Group4App.swift
@@ -0,0 +1,17 @@
+//
+//  Group4App.swift
+//  Group4
+//
+//  Created by Tae Yoon Kim on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Group4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Group4Tests/Group4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Group4Tests.swift
+//  Group4Tests
+//
+//  Created by Tae Yoon Kim on 6/12/25.
+//
+
+import Testing
+@testable import Group4
+
+struct Group4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
